e216bc8757523d249cab2753fcfa8dd25616a013
волкова елизавета <esvolkova@edu.hse.ru>
2026-06-03 15:15:31 +0300

    Реализован класс Recipe с методами масштабирования и валидации, который представляет собой рецепт одного блюда

--- recipes.py
+++ recipes.py
@@ -28,3 +28,45 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return NotImplemented
         return self.name == other.name and self.unit == other.unit
+    
+
+class Recipe:
+    """Представляет рецепт одного блюда."""
+    def __init__(self, title: str, ingredients: list):
+        if not isinstance(ingredients, list):
+            raise TypeError("ingredients должен быть списком объектов Ingredient")
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient: Ingredient):
+        """Добавляет ингредиент. Если такой уже есть, суммирует количество."""
+        for item in self.ingredients:
+            if item == ingredient:
+                item.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        """Проверяет, что ratio — число и больше нуля."""
+        return type(ratio) in (int, float) and ratio > 0
+
+    def scale(self, ratio: float) -> 'Recipe':
+        """Возвращает новый масштабированный рецепт. Исходный не меняется."""
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэф. должен быть положительным числом")
+        
+        new_ingredients = [
+            Ingredient(i.name, i.quantity*ratio, i.unit)
+            for i in self.ingredients
+        ]
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self) -> int:
+        """Возвращает количество уникальных ингредиентов."""
+        return len(self.ingredients)
+
+    def __str__(self) -> str:
+        """Возвращает читаемое представление рецепта."""
+        ingredients = "\n".join(f"{ingr}" for ingr in self.ingredients)
+        return f"Рецепт: {self.title}\n{ingredients}"
